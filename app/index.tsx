042ec439089e4d8ae81373f3eb897aedc40f4f95
number6823 <ohdehet@naver.com>
2026-06-19 10:36:03 +0900

    React-native 기본셋팅

---
+++ app/index.tsx
@@ -0,0 +1,10 @@
+// /app/index.tsx
+import { Text } from "react-native";
+
+function HomeScreen() {
+    return (
+        <Text>Initialization</Text>
+    )
+}
+
+export default HomeScreen;
---
+++ metro.config.js
@@ -0,0 +1,7 @@
+// /metro.config.js
+const { getDefaultConfig } = require("expo/metro-config");
+const { withNativeWind } = require("nativewind/metro");
+
+const config = getDefaultConfig(__dirname);
+config.resolver.unstable_enablePackageExports = false;
+module.exports = withNativeWind(config, { input: "./styles/global.css" });
